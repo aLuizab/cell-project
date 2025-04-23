7e723ae8895d56214651ef7ef8900db815078c85
aLuizab <aluiza.primo@gmail.com>
2025-04-23 17:30:54 -0300

    refactor: criando apenas a vpc

--- terraform/cell-a/main.tf
+++ terraform/cell-a/main.tf
@@ -1,23 +1,9 @@
-module "vpc_cell_as" {
+module "vpc_cell_a" {
   source               = "../modules/vpc"
-  name                 = "cell-as"
+  name                 = "cell-a"
   cidr_block           = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
-  azs                  = ["us-east-2a", "us-east-2b"]
-}
-
-module "eks_cell_as" {
-  source                = "../modules/eks"
-  cluster_name          = "cell-as"
-  cluster_version       = "1.29"
-  vpc_id                = module.vpc_cell_as.vpc_id
-  subnet_ids            = module.vpc_cell_as.private_subnet_ids
-  node_desired_capacity = 2
-  node_max_capacity     = 3
-  node_min_capacity     = 1
-  node_instance_type    = "t3.medium"
-  cell_name             = "cell-as"
-  kms_key_arn = "arn:aws:kms:us-east-2:931157493096:key/34a394e3-fafd-4b7f-a1ba-2fc8a77ed4e7"
+  azs                  = ["us-east-2a", "us-east-2b","us-east-1a", "us-east-1b"]
 }
 
--- terraform/cell-b/main.tf
+++ terraform/cell-b/main.tf
@@ -4,18 +4,5 @@ module "vpc_cell_b" {
   cidr_block           = "10.1.0.0/16"
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
   private_subnet_cidrs = ["10.1.101.0/24", "10.1.102.0/24"]
-  azs                  = ["us-east-2a", "us-east-2b"]
-}
-
-module "eks_cell_a" {
-  source                = "../modules/eks"
-  cluster_name          = "eks-cell-b"
-  cluster_version       = "1.29"
-  vpc_id                = module.vpc_cell_b.vpc_id
-  subnet_ids            = module.vpc_cell_b.private_subnet_ids
-  node_desired_capacity = 2
-  node_max_capacity     = 3
-  node_min_capacity     = 1
-  node_instance_type    = "t3.medium"
-  cell_name             = "cell-b"
+  azs                  = ["us-east-2a", "us-east-2b", "us-east-1a", "us-east-1b"]
 }
