1643c86da007f470b4c0c51fd93086bbbbd95db6
aLuizab <aluiza.primo@gmail.com>
2025-04-23 10:29:34 -0300

    refactor: refazendo o modulo eks

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -11,52 +11,44 @@ data "aws_availability_zones" "available" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.36.0"
+  version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.31"
 
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-  create_kms_key                           = false
-
+  bootstrap_self_managed_addons = false
   cluster_addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
   }
 
-  vpc_id     = var.vpc_id
-    subnet_ids = var.subnet_ids
+  # Optional
+  cluster_endpoint_public_access = true
 
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.subnet_ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
 
   eks_managed_node_groups = {
-      default = {
-        desired_capacity = var.node_desired_capacity
-        max_capacity     = var.node_max_capacity
-        min_capacity     = var.node_min_capacity
-        instance_types   = [var.node_instance_type]
-      }
-    }
-}
+    default = {
+            desired_capacity = var.node_desired_capacity
+            max_capacity     = var.node_max_capacity
+            min_capacity     = var.node_min_capacity
+            instance_types   = [var.node_instance_type]
+          }
+  }
 
-# The Amazon Elastic Block Store (Amazon EBS) Container Storage Interface (CSI) driver manages the lifecycle of Amazon EBS volumes as storage for the Kubernetes Volumes that you create.
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
