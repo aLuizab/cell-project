9845f1542c814d08cab54ed66d234cc348731af7
aLuizab <aluiza.primo@gmail.com>
2025-04-23 23:23:33 -0300

    refactor: refatorando modulo eks

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,65 +1,30 @@
-provider "aws" {
-  region = var.region
-}
-
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+ source  = "terraform-aws-modules/eks/aws"
+ version = "~> 20.31"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.31"
+ cluster_name    = var.cluster_name
+ cluster_version = "1.31"
 
-  bootstrap_self_managed_addons = false
-  cluster_addons = {
-    coredns = {
-      resolve_conflicts_on_create = "OVERWRITE"
-      addon_version               = "v1.10.1-eksbuild.3"
+ # Optional
+ cluster_endpoint_public_access = true
 
-      timeouts = {
-        create = "25m"
-        delete = "10m"
-      }
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      addon_version = "v1.14.1-eksbuild.1"
-    }
-  }
+ # Optional: Adds the current caller identity as an administrator via cluster access entry
+ enable_cluster_creator_admin_permissions = true
 
-  # Optional
-  cluster_endpoint_public_access = true
+ eks_managed_node_groups = {
+   default = {
+     instance_types = ["t3.medium"]
+     min_size       = 1
+     max_size       = 3
+     desired_size   = 2
+   }
+ }
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
+ vpc_id     = var.vpc_id
+ subnet_ids = var.subnet_ids
 
-  vpc_id                   = var.vpc_id
-  subnet_ids               = var.subnet_ids
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
-  eks_managed_node_groups = {
-    default = {
-            desired_capacity = var.node_desired_capacity
-            max_capacity     = var.node_max_capacity
-            min_capacity     = var.node_min_capacity
-            instance_types   = [var.node_instance_type]
-          }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+ tags = {
+   Environment = "dev"
+   Terraform   = "true"
+ }
 }
